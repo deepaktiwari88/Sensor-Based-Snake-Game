841ea131909c209537a180146362209d600bb309
MadhavPruthi <madhavpruthi.becse16@pec.edu.in>
2019-11-07 11:55:14 +0530

    Added Sensor Motion Capabilities

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -18,13 +18,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+
+  static int level = 1;
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+
   @override
   Widget build(BuildContext context) {
+
     return new Scaffold(
         backgroundColor: Color(0xFFecc073),
-        body: new Center(
-          child: new Game(),
-        ));
+        body: Stack(children: <Widget>[
+
+          Positioned(
+            child: Container(
+                height: 50.0,
+                width: 300.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white.withOpacity(0.6),
+
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Level: " + Home.level.toString(),
+                        style: TextStyle(
+                          color: Colors.blueGrey.withOpacity(0.8),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 28.0,
+                        )),
+                  ],
+                )),
+            top: 100.0,
+            left: 50.0,
+          ),
+          new Center(
+            child: new Game(),
+          ),
+        ]));
   }
 }
